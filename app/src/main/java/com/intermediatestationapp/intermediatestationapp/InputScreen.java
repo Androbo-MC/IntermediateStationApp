d297c7ce7dcf4aaaf1c913de4bef5578c2edfc79
sekiguchinatsuo <n.sekiguchi@androbo.jp>
2017-10-21 15:33:39 +0900

    入力画面を記述（作成中）

--- app/src/main/java/com/intermediatestationapp/intermediatestationapp/InputScreen.java
+++ app/src/main/java/com/intermediatestationapp/intermediatestationapp/InputScreen.java
@@ -1,5 +1,6 @@
 package com.intermediatestationapp.intermediatestationapp;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.TextureView;
@@ -14,15 +15,18 @@ import java.util.ArrayList;
  */
 public class InputScreen extends AppCompatActivity {
 
-    //入力欄をArrayListに格納
-    private ArrayList<EditText> inputBoxList = new ArrayList<>();
     //カウンター
     private int counter = 0;
-    //入力値をArrayListに格納
+    //入力欄を格納するリスト
+    private ArrayList<EditText> inputBoxList = new ArrayList<>();
+    //入力値を格納するリスト
     private ArrayList<String> inputStationNameList = new ArrayList<>();
+    // プログレスバー
+    private ProgressDialog dialog = null;
+
 
     /**
-     * 初期状態でactivityを作成
+     * 初期処理
      *
      * @param savedInstanceState
      */
@@ -31,11 +35,15 @@ public class InputScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_screen);
 
-        //入力されたテキストボックスの値を取得し、配列リストに設定
-        //inputBoxList.add((EditText) findViewById(R.id.inputBox1));
-        //カウンターを初期化
+        // 入力欄をリストに格納
+        inputBoxList.add((EditText) findViewById(R.id.inputBox1));
+        inputBoxList.add((EditText) findViewById(R.id.inputBox2));
+        inputBoxList.add((EditText) findViewById(R.id.inputBox3));
+        inputBoxList.add((EditText) findViewById(R.id.inputBox4));
+        inputBoxList.add((EditText) findViewById(R.id.inputBox5));
+        // カウンターを初期化
         counter = 0;
-        //検索ボタンを取得
+        // 検索ボタンを取得
         Button searchButton = (Button)findViewById(R.id.searchButton);
         searchButton.setOnClickListener((View.OnClickListener) this);
     }
@@ -46,20 +54,72 @@ public class InputScreen extends AppCompatActivity {
      */
     public void onClick(View V) {
 
-        //入力欄の値を取得する
-        for (EditText inputBox : inputBoxList) { //入力値をinputBoxListに格納
-            if (!inputBox.getText().toString().isEmpty()); //空のテキストボックスではないとき
-
-                inputStationNameList.add(inputBox.getText().toString()); //inputStationNameListに
-        }
-
-        //
-        for (String inputBox : inputStationNameList) {
-            if (null != inputBox && !inputBox.isEmpty()) {
-                counter++;
-
+        // 入力値をリストに格納
+        for (EditText text : inputBoxList) {
+            // 空の入力欄は含まない
+            if (!text.getText().toString().isEmpty()) {
+                inputStationNameList.add(text.getText().toString());
             }
         }
 
+        // リクエストする
+        for (String text : inputStationNameList){
+            if (null != text && !text.isEmpty()){
+                counter++;
+                getPlaceInfo(text);
+            }
+        }
+    }
+
+    /**
+     * 位置情報を取得
+     */
+    private void getPlaceInfo(){
+
+        // TODO : アニメーションを表示させる
+        // プログレスバーを表示
+        if (counter == 1){
+            dialog = new ProgressDialog(InputScreen.this);
+            dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            dialog.setMessage("検索中");
+            dialog.setCancelable(false);
+            dialog.show();
+        }
+    }
+
+    /**
+     * クリアボタン押下時の処理
+     */
+    @Override
+     protected void onCreate (Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.id.activity_input_screen);
+        final EditText inputBox = (EditText) findViewById(R.id.inputBox);
+
+        Button clearButton = (Button) findViewById(R.id.clearButton);
+        clearButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                inputBox.setText("");
+            }
+        });
+
+    }
+
+    /**
+     * 設定ボタン押下後の処理
+     */
+    protected void onCreate (Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView((R.id.avtivity_input_screen));
+
+        Button settingButton = (Button) findViewById(R.id.settingButton);
+        settingButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                // TODO : 設定画面表示処理を記述する
+            }
+        });
     }
 }
+
