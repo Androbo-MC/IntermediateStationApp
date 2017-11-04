345985f703d3cbc574ed8d6a4a8a49782131399e
sekiguchinatsuo <n.sekiguchi@androbo.jp>
2017-11-04 16:15:00 +0900

    [関口]入力画面のxml、内部処理を修正

--- app/src/main/java/com/intermediatestationapp/intermediatestationapp/InputScreen.java
+++ app/src/main/java/com/intermediatestationapp/intermediatestationapp/InputScreen.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 /**
  * 入力画面
  */
-public class InputScreen extends AppCompatActivity {
+public class InputScreen extends AppCompatActivity implements View.OnClickListener{
 
     //カウンター
     private int counter = 0;
@@ -22,7 +22,7 @@ public class InputScreen extends AppCompatActivity {
     //入力値を格納するリスト
     private ArrayList<String> inputStationNameList = new ArrayList<>();
     // プログレスバー
-    private ProgressDialog dialog = null;
+//    private ProgressDialog dialog = null;
 
 
     /**
@@ -41,19 +41,99 @@ public class InputScreen extends AppCompatActivity {
         inputBoxList.add((EditText) findViewById(R.id.inputBox3));
         inputBoxList.add((EditText) findViewById(R.id.inputBox4));
         inputBoxList.add((EditText) findViewById(R.id.inputBox5));
+
         // カウンターを初期化
         counter = 0;
+
         // 検索ボタンを取得
         Button searchButton = (Button)findViewById(R.id.searchButton);
-        searchButton.setOnClickListener((View.OnClickListener) this);
+        searchButton.setOnClickListener(this);
+
+        // 設定ボタンを取得
+        Button settingButton = (Button)findViewById(R.id.settingButton);
+        settingButton.setOnClickListener(this);
+
+        // クリアボタンを取得
+        Button clearButton = (Button)findViewById(R.id.clearButton);
+        clearButton.setOnClickListener(this);
     }
 
     /**
      * 検索ボタン押下時の処理
      * @param V
      */
-    public void onClick(View V) {
+//    public void SearchButtonOnClick(View V) {
+//
+//        // 入力値をリストに格納
+//        for (EditText text : inputBoxList) {
+//            // 空の入力欄は含まない
+//            if (!text.getText().toString().isEmpty()) {
+//                inputStationNameList.add(text.getText().toString());
+//            }
+//        }
+//
+//        // 位置情報取得メソッドに入力値を渡す。
+//        for (String text : inputStationNameList){
+//            if (null != text && !text.isEmpty()){
+//                counter++;
+//                getPlaceInfo(text);
+//            }
+//        }
+//    }
 
+    /**
+     * 位置情報を取得
+     */
+    private void getPlaceInfo(String text){
+
+        // TODO : プログレスバーは仮。アニメーションを表示するよう変更する。
+        // プログレスバーを表示
+//        if (counter == 1){
+//            dialog = new ProgressDialog(InputScreen.this);
+//            dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+//            dialog.setMessage("検索中");
+//            dialog.setCancelable(false);
+//            dialog.show();
+//        }
+    }
+
+    /**
+     * クリアボタン押下時の処理
+     */
+    private void ClearButtonOnClick(View V){
+
+        // 入力欄1をクリアする。
+        EditText inputBox1 = new EditText(this);
+        inputBox1.getEditableText().clear();
+
+        // 入力欄2をクリアする。
+        EditText inputBox2 = new EditText(this);
+        inputBox2.getEditableText().clear();
+
+        // 入力欄3をクリアする。
+        EditText inputBox3 = new EditText(this);
+        inputBox3.getEditableText().clear();
+
+        // 入力欄4をクリアする。
+        EditText inputBox4 = new EditText(this);
+        inputBox4.getEditableText().clear();
+
+        // 入力欄5をクリアする。
+        EditText inputBox5 = new EditText(this);
+        inputBox5.getEditableText().clear();
+
+    }
+
+
+    /**
+     * 設定ボタン押下後の処理
+     */
+    private void SettingButtonOnClick(View V){
+        // TODO:設定画面を表示する処理を記述する。
+    }
+
+    @Override
+    public void onClick(View view) {
         // 入力値をリストに格納
         for (EditText text : inputBoxList) {
             // 空の入力欄は含まない
@@ -62,64 +142,14 @@ public class InputScreen extends AppCompatActivity {
             }
         }
 
-        // リクエストする
+        // 位置情報取得メソッドに入力値を渡す。
         for (String text : inputStationNameList){
             if (null != text && !text.isEmpty()){
                 counter++;
                 getPlaceInfo(text);
             }
         }
-    }
 
-    /**
-     * 位置情報を取得
-     */
-    private void getPlaceInfo(){
-
-        // TODO : アニメーションを表示させる
-        // プログレスバーを表示
-        if (counter == 1){
-            dialog = new ProgressDialog(InputScreen.this);
-            dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-            dialog.setMessage("検索中");
-            dialog.setCancelable(false);
-            dialog.show();
-        }
-    }
-
-    /**
-     * クリアボタン押下時の処理
-     */
-    @Override
-     protected void onCreate (Bundle saveInstanceState){
-        super.onCreate(saveInstanceState);
-        setContentView(R.id.activity_input_screen);
-        final EditText inputBox = (EditText) findViewById(R.id.inputBox);
-
-        Button clearButton = (Button) findViewById(R.id.clearButton);
-        clearButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                inputBox.setText("");
-            }
-        });
-
-    }
-
-    /**
-     * 設定ボタン押下後の処理
-     */
-    protected void onCreate (Bundle saveInstanceState){
-        super.onCreate(saveInstanceState);
-        setContentView((R.id.avtivity_input_screen));
-
-        Button settingButton = (Button) findViewById(R.id.settingButton);
-        settingButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                // TODO : 設定画面表示処理を記述する
-            }
-        });
     }
 }
 
