c9b45932c8e093baa2fbd9e59b0a6eeae0a2699f
Androbo-MC <n.sekiguchi@androbo.jp>
2017-11-26 00:48:14 +0900

    [川﨑]ボタンの位置を修正

---
+++ app/src/main/java/com/intermediatestationapp/intermediatestationapp/utils/CalculateUtil.java
@@ -0,0 +1,8 @@
+package com.intermediatestationapp.intermediatestationapp.utils;
+
+/**
+ * 計算共通クラス
+ */
+
+public class CalculateUtil {
+}
---
+++ app/src/main/java/com/intermediatestationapp/intermediatestationapp/utils/IntentUtil.java
@@ -0,0 +1,8 @@
+package com.intermediatestationapp.intermediatestationapp.utils;
+
+/**
+ * 画面遷移準備共通クラス
+ */
+
+public class IntentUtil {
+}
