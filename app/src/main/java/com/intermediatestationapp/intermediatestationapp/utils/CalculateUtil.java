eaccf79c7fda0e2832031bcc99939c40d4dcf1d8
Androbo-MC <n.sekiguchi@androbo.jp>
2017-12-01 23:50:03 +0900

    [川﨑]周辺情報画面用処理を追加

--- app/src/main/java/com/intermediatestationapp/intermediatestationapp/utils/CalculateUtil.java
+++ app/src/main/java/com/intermediatestationapp/intermediatestationapp/utils/CalculateUtil.java
@@ -1,8 +1,19 @@
 package com.intermediatestationapp.intermediatestationapp.utils;
 
+
 /**
  * 計算共通クラス
  */
 
 public class CalculateUtil {
+
+    /**
+     * 中間地点を計算するメソッド
+     *
+     * @param latList 計算に使う全駅の緯度リスト
+     * @param lngList 計算に使う全駅の経度リスト
+     * @return LatLng 中間地点の座標オブジェクト
+     */
+
+
 }
