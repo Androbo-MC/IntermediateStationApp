be1bbebf0dbbcf3f20daa39c84d32ee0982b4ba2
Androbo-MC <n.sekiguchi@androbo.jp>
2017-12-02 00:10:32 +0900

    [川﨑]1都6県の駅一覧用を更新

--- app/src/main/java/com/intermediatestationapp/intermediatestationapp/storage/MyOpenHelper.java
+++ app/src/main/java/com/intermediatestationapp/intermediatestationapp/storage/MyOpenHelper.java
@@ -29,6 +29,12 @@ public class MyOpenHelper extends SQLiteOpenHelper {
         // DB作成時に駅データをまとめて取り込む(1都6県の全駅)
         ArrayList<StationVO> stationVOList = new ArrayList<>();
         stationVOList.add(new StationVO("相老","あいおい","10","36.410261","139.304178","0001780","相老（群馬）"));
+        stationVOList.add(new StationVO("愛甲石田","あいこういしだ","14","35.417634","139.344098","0003042","愛甲石田"));
+        stationVOList.add(new StationVO("相原","あいはら","13","35.60694","139.33169","0002754","相原"));
+        stationVOList.add(new StationVO("青井","あおい","13","35.771782","139.82038","0002713","青井"));
+        stationVOList.add(new StationVO("青砥","あおと","13","35.745883","139.856292","0002716","青砥"));
+        stationVOList.add(new StationVO("青葉台","あおばだい","14","35.542855","139.517376","0003216","青葉台"));
+        stationVOList.add(new StationVO("青堀","あおほり","12","35.330304","139.858139","0002237","青堀"));
         stationVOList.add(new StationVO("蕨","わらび","11","35.827959","139.690357","0002044","蕨"));
 
         // VOに取り込んだ駅データをテーブルにすべて挿入
